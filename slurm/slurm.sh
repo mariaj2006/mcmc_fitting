0558c0ad6d96747ab4d5eba63cf00cafa92f8611
maria <msanchezrincon@carnegiescience.edu>
2025-07-29 15:40:30 -0700

    add slurm

---
+++ slurm/slurm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name="velocity_fitting"
+#SBATCH --time=10:00:00
+#SBATCH --array=1-2
+#SBATCH --mem=2G
+#SBATCH --output=out-array_%A_%a.out  
+#SBATCH --error=err-array_%A_%a.err  
+#SBATCH --partition=obs
+#SBATCH --ntasks=1
+
+
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+module load python
+conda activate emcee_fitting
+
+python3 O3_1comp.py $SLURM_ARRAY_TASK_ID
+
+
+
+
+
+
+
+
+
